131498fbe8edf2287cf7bcb182a4035625c08ae9
Pakyin Wong <pakyinwww@gmail.com>
2019-09-04 12:58:46 +0800

    PixiCanvas added.

--- src/component/PixiCanvas.js
+++ src/component/PixiCanvas.js
@@ -1,10 +1,46 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import * as PIXI from 'pixi.js';
+import styles from './PixiCanvas.css'
 
 const PixiCanvas = () => {
     const canvas = useRef();
+
+    useEffect(() => {
+        const app = new PIXI.Application({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            transparent: true
+        });
+        canvas.current.appendChild(app.view)
+
+        // Load the bunny texture
+        app.loader.add('bunny', 'https://pixijs.io/examples/examples/assets/bunny.png').load(startup);
+
+        function startup() {
+            var bunny = new PIXI.Sprite(app.loader.resources.bunny.texture);
+
+            // Center the sprite's anchor point
+            bunny.anchor.set(0.5);
+
+            // Move the sprite to the center of the screen
+            bunny.x = app.renderer.width / 2;
+            bunny.y = app.renderer.height / 2;
+
+            app.stage.addChild(bunny);
+
+            // Listen for animate update
+            app.ticker.add(function (delta) {
+                // Rotate mr rabbit clockwise
+                bunny.rotation += 0.1 * delta;
+            });
+        }
+
+    })
+
+
     return (
         <>
-            <div ref={canvas}>
+            <div ref={canvas} className={styles.pixicanvas}>
             </div>
         </>
     );
--- src/screen/Lobby.js
+++ src/screen/Lobby.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../../node_modules/video-react/dist/video-react.css";
 import { Player } from "video-react";
 import PixiCanvas from "./../component/PixiCanvas";
+import styles from './Lobby.css'
+
 
 const Lobby = () => {
   return (
     <>
-      <Player>
+      <Player className={styles.player}>
         <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
       </Player>
       <PixiCanvas>
