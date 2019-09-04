d6e4d326cec656cf88e401c2861b4361db62116e
Wong Pak Yin <pakyinwww@gmail.com>
2019-09-04 10:25:53 +0800

    Project init.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Lobby from './screen/Lobby'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Lobby></Lobby>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screen/Lobby.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Lobby = () => {
+    return ( <> </> )
+}
+
+export default Lobby
